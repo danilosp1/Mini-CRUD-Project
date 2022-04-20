067dae0522d673d32bc7926b7b51846dcc6e740a
Danilo da Silva Pinto <danilo.sp03@gmail.com>
2022-04-20 18:52:19 -0300

    Finalizando

--- main.js
+++ main.js
@@ -8,13 +8,6 @@ const closeModal = () => {
     document.getElementById('modal').classList.remove('active')
 }
 
-const tempClient = {
-    nome: "Danilo",
-    email: "email@email.com",
-    celular: "12345679090",
-    cidade: "Amazonas"
-}
-
 const getLocalStorage = () => JSON.parse(localStorage.getItem('db_client')) ?? []
 const setLocalStorage = (dbClient) => localStorage.setItem("db_client", JSON.stringify(dbClient))
 
@@ -23,6 +16,7 @@ const deleteClient = (index) => {
     const dbClient = readClient()
     dbClient.splice(index, 1)
     setLocalStorage(dbClient)
+    updateTable()
 }
 
 const updateClient = (index, client) => {
@@ -57,10 +51,75 @@ const saveClient = () => {
             celular: document.getElementById('celular').value,
             cidade: document.getElementById('cidade').value
         }
-        createClient(client)
+        const index = document.getElementById('nome').dataset.index
+        if(index == 'new'){
+            createClient(client)
+        } else {
+            updateClient(index, client)
+        }
+        updateTable()
         closeModal()
     }
 }
+
+const createRow = (client, index) => {
+    const newRow = document.createElement('tr')
+    newRow.innerHTML = `
+        <td>${client.nome}</td>
+        <td>${client.email}</td>
+        <td>${client.celular}</td>
+        <td>${client.cidade}</td>
+        <td>
+            <button type="button" class="button green" data-action="edit-${index}">editar</button>
+            <button type="button" class="button red" data-action="delete-${index}">excluir</button>
+        </td>
+    `
+    document.querySelector('#tableClient>tbody').appendChild(newRow)
+}
+
+const clearTable = () => {
+    const rows = document.querySelectorAll('#tableClient>tbody tr')
+    rows.forEach(row => row.parentNode.removeChild(row))
+}
+
+const updateTable = () => {
+    const dbClient = readClient()
+    clearTable()
+    dbClient.forEach(createRow)
+}
+
+const fillFields = (client) => {
+    document.getElementById('nome').value = client.nome
+    document.getElementById('email').value = client.email
+    document.getElementById('celular').value = client.celular
+    document.getElementById('cidade').value = client.cidade
+    document.getElementById('nome').dataset.index = client.index
+}
+
+const editClient = (index) => {
+    const client = readClient()[index]
+    client.index = index
+    fillFields(client)
+    openModal()
+}
+
+const editDelete = (event) => {
+    if(event.target.type == 'button'){
+        const [action, index] = event.target.dataset.action.split('-')
+
+        if(action == 'edit'){
+            editClient(index)
+        } else {
+            const client = readClient()[index]
+            const response = confirm(`Deseja realmente excluir o cliente ${client.nome}`)
+            if(response){
+                deleteClient(index)
+            }
+        }
+    }
+}
+
+updateTable()
 
 // Events
 document.getElementById('cadastrarCliente')
@@ -74,3 +133,6 @@ document.getElementById('salvar')
 
 document.getElementById('cancelar')
     .addEventListener('click', closeModal)
+
+document.querySelector('#tableClient>tbody')
+    .addEventListener('click', editDelete)
